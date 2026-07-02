fd5e0f7e6d1e1d5894a1165766b5e5f1030241a2
Anna Testova <testova.annamaks@gmail.com>
2026-07-02 18:46:34 +0200

    Delete GreenWashing directory

--- GreenWashing/src/extraction.py
+++
@@ -1,8 +0,0 @@
-from nltk.tokenize import sent_tokenize
-
-def extract_text_from_file(path):
-    with open(path, "r", encoding="utf-8") as file:
-        return file.read()
-
-def split_into_sentences(text):
-    return sent_tokenize(text)
